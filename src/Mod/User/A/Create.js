ef78e477a7c584fb49003f90a5538f0c3d45b4a0
Alex Gusev <alex@flancer64.com>
2024-10-23 15:50:28 +0300

    WF-739 fix user registration with tg bot

--- src/Mod/User/A/Create.js
+++ src/Mod/User/A/Create.js
@@ -1,5 +1,5 @@
 /**
- * Create new user in RDB.
+ * Create a new user in the RDB.
  *
  * @implements TeqFw_Core_Shared_Api_Act
  */
@@ -8,27 +8,28 @@ export default class Demo_Back_Mod_User_A_Create {
      * @param {TeqFw_Db_Back_Api_RDb_CrudEngine} crud
      * @param {Demo_Back_Store_RDb_Schema_User} rdbUser
      */
-    constructor(
-        {
-            TeqFw_Db_Back_Api_RDb_CrudEngine$: crud,
-            Demo_Back_Store_RDb_Schema_User$: rdbUser,
-        }
-    ) {
-        // VARS
+    constructor({
+                    TeqFw_Db_Back_Api_RDb_CrudEngine$: crud,
+                    Demo_Back_Store_RDb_Schema_User$: rdbUser
+                }) {
         const A_USER = rdbUser.getAttributes();
 
-        // MAIN
         /**
+         * Create a new user in the database.
+         *
          * @param {Object} params
          * @param {TeqFw_Db_Back_RDb_ITrans} params.trx
          * @param {Demo_Back_Store_RDb_Schema_User.Dto} params.dbUser
-         *
          * @return {Promise<number>}
+         * @throws {Error} if `telegram_id` or `telegram_user` are missing.
          */
-        this.act = async function ({trx, dbUser}) {
+        this.act = async ({trx, dbUser}) => {
+            const {telegram_id, telegram_user} = dbUser;
+            if (!telegram_id || !telegram_user) {
+                throw new Error('Cannot register new Telegram user without name or ID.');
+            }
             const {[A_USER.ID]: id} = await crud.create(trx, rdbUser, dbUser);
             return id;
         };
     }
-
 }
